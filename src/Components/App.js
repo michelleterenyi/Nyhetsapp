189069269e22ee1c1bf2ccfd004f1bb5c57b0923
Michelle Terenyi <miselke98@gmail.com>
2019-05-28 10:05:18 +0200

    fixat

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import News from './News/News';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1 className="App-title">Nyheter</h1>
-        </a>
-      </header>
-      <News />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">My feed</h1>
+        </header>
+        <News />
+      </div>
+    );
+  }
 }
 
 export default App;
